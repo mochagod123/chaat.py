37ddc37b0134e2bd6094a99e285bd2e950c86c41
暇な人 <yuuta4083@gmail.com>
2025-03-19 10:16:19 +0900

    大幅な仕様変更。room.sendみたいにした。

--- chatroom.py
+++ chatroom.py
@@ -25,14 +25,6 @@ class UserSetting():
     def color(self):
         return self._color
     
-class Message():
-    def __init__(self, content: str):
-        self._content = content
-
-    @property
-    def content(self):
-        return self._content
-    
 class Chaat:
     def __init__(self, hashid: str = None):
         self.ses = requests.Session()
@@ -154,8 +146,120 @@ class Command:
         self.name = name
         self.func = func
 
-    async def invoke(self, *args):
-        return await self.func(*args)
+    async def invoke(self, ctx, *args):
+        return await self.func(ctx, *args)
+
+class Http:
+    def __init__(self, session: requests.Session, token: str):
+        self.ses = session
+        self.token_pat = r"[a-zA-Z0-9]{32}"
+        self.token = token
+        pass
+
+    async def create_room(self):
+        loop = asyncio.get_event_loop()
+        data = {
+            'action': 'createRoom',
+            'csrf_token_check': self.token,
+        }
+
+        response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/api_server.php', data=data))
+
+        js = response.json()
+
+        return js
+
+    async def send_room(self, text: str, hash: str):
+        loop = asyncio.get_event_loop()
+        data = {
+            'action': 'sendData',
+            'hash': hash,
+            'profile_name': '匿名とむ',
+            'profile_color': '#000000',
+            'data': '{"type":"chat","msg":"' + text +'"}',
+            'csrf_token_check': self.token,
+        }
+
+        response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
+        return response.json()
+        
+    async def edit_user(self, usersetting: UserSetting, hash: str):
+        loop = asyncio.get_event_loop()
+        data = {
+            'action': 'changeMyProfile',
+            'hash': hash,
+            'new_name': usersetting.name,
+            'new_trip': '',
+            'new_color': usersetting.color,
+            'csrf_token_check': self.token,
+        }
+
+        response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
+
+        return response.json()
+        
+    def generate_current_timestamp(self):
+        now = datetime.datetime.now()
+        return now.strftime("%Y%m%d%H%M%S")
+
+    async def fetch_messages(self, hash: str):
+        loop = asyncio.get_event_loop()
+        data = {
+            'action': 'fetchData',
+            'hash': hash,
+            'csrf_token_check': self.token,
+                'mode': 'log',
+                'type': 'last',
+                'num': self.generate_current_timestamp(),
+        }
+
+        response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
+
+        return response.json()
+
+class Room():
+    def __init__(self, hash: str):
+        self._hash = hash
+        pass
+
+    async def send(self, text: str):
+        return await http_req.send_room(text, self._hash)
+    
+    async def messages(self):
+        return await http_req.fetch_messages(self._hash)
+    
+    async def edit_user(self, user: UserSetting):
+        return await http_req.edit_user(user, self._hash)
+    
+    @property
+    def hash(self):
+        return self._hash
+    
+    @property
+    def url(self):
+        return "https://c.kuku.lu/" + self._hash
+
+class Message():
+    def __init__(self, content: str, room: Room):
+        self._content = content
+        self._room = room
+
+    @property
+    def content(self):
+        return self._content
+    
+    @property
+    def room(self):
+        return self._room
+
+class Context:
+    def __init__(self, room: Room):
+        self._room = room
+        pass
+
+    @property
+    def room(self):
+        return self._room
 
 class WebSocket():
     def __init__(self, hashid: str = None):
@@ -165,6 +269,7 @@ class WebSocket():
         self.token = None
         self.events = {}
         self.commands = {}
+        self.rooms = []
 
     def url_to_hash(self, url: str):
         return url.replace("https://c.kuku.lu", "").replace("/", "")
@@ -184,7 +289,7 @@ class WebSocket():
             return func
         return decorator
 
-    async def process_command(self, message):
+    async def process_command(self, message: Message):
         parts = message.content.split()
         if not parts:
             return
@@ -193,7 +298,7 @@ class WebSocket():
         args = parts[1:]
 
         if cmd_name in self.commands:
-            await self.commands[cmd_name].invoke(*args)
+            await self.commands[cmd_name].invoke(Context(message.room), *args)
         else:
             return
 
@@ -208,16 +313,19 @@ class WebSocket():
             while True:
                 message = await websocket.recv()
                 data = json.loads(message.replace("@", "", 1))
+                print(data)
                 if data["type"] == "data":
                     d = data["data"]
                     if d["type"] == "chat":
                         msg = d["msg"]
-                        await self.dispatch("on_chat", Message(msg))
+                        await self.dispatch("on_chat", Message(msg, Room(hash_)))
 
     def login(self, hashid: str = None):
         res = self.ses.get("https://c.kuku.lu/")
         token = re.search(self.token_pat, res.text).group()
         self.token = token
+        global http_req
+        http_req = Http(self.ses, self.token)
         if hashid:
             self.hash_id = hashid
         if self.hash_id:
@@ -235,99 +343,25 @@ class WebSocket():
                 asyncio.run(self.receive_messages(matches, hashid))
             else:
                 return "エラー"
-            
-    async def send_room(self, text: str, hash: str = None):
+
+    async def join_room(self, room: Room):
         loop = asyncio.get_event_loop()
-        if hash:
-            data = {
-                'action': 'sendData',
-                'hash': hash,
-                'profile_name': '匿名とむ',
-                'profile_color': '#000000',
-                'data': '{"type":"chat","msg":"' + text +'"}',
-                'csrf_token_check': self.token,
-            }
+        res = await loop.run_in_executor(None, partial(self.ses.post, "https://c.kuku.lu/"))
+        token = re.search(self.token_pat, res.text).group()
+        self.token = token
+        j = await loop.run_in_executor(None, partial(self.ses.post, f"https://c.kuku.lu/{room.hash}"))
+        pattern = r"[a-zA-Z0-9+/=]{88}"
+        matches = re.findall(pattern, j.text)[1]
+        await self.receive_messages(matches, room.hash)
 
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
+    async def fetch_rooms(self):
+        return self.rooms
+    
+    async def fetch_room(self, hash: str):
+        return Room(hash)
 
-            return response.json()
-        else:
-            if not self.hash_id:
-                return {"Error": "ハッシュIDが設定されていません。"}
-            data = {
-                'action': 'sendData',
-                'hash': self.hash_id,
-                'profile_name': '匿名とむ',
-                'profile_color': '#000000',
-                'data': '{"type":"chat","msg":"' + text +'"}',
-                'csrf_token_check': self.token,
-            }
-
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
-
-            return response.json()
-        
-    async def edit_user(self, usersetting: UserSetting, hash: str = None):
-        loop = asyncio.get_event_loop()
-        if hash:
-            data = {
-                'action': 'changeMyProfile',
-                'hash': hash,
-                'new_name': usersetting.name,
-                'new_trip': '',
-                'new_color': usersetting.color,
-                'csrf_token_check': self.token,
-            }
-
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
-
-            return response.json()
-        else:
-            if not self.hash_id:
-                return {"Error": "ハッシュIDが設定されていません。"}
-            data = {
-                'action': 'changeMyProfile',
-                'hash': self.hash_id,
-                'new_name': usersetting.name,
-                'new_trip': '',
-                'new_color': usersetting.color,
-                'csrf_token_check': self.token,
-            }
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
-
-            return response.json()
-        
-    def generate_current_timestamp(self):
-        now = datetime.datetime.now()
-        return now.strftime("%Y%m%d%H%M%S")
-
-    async def fetch_room(self, hash: str = None):
-        loop = asyncio.get_event_loop()
-        if hash:
-            data = {
-                'action': 'fetchData',
-                'hash': hash,
-                'csrf_token_check': self.token,
-                'mode': 'log',
-                'type': 'last',
-                'num': self.generate_current_timestamp(),
-            }
-
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
-
-            return response.json()
-        else:
-            if not self.hash_id:
-                return {"Error": "ハッシュIDが設定されていません。"}
-            data = {
-                'action': 'fetchData',
-                'hash': self.hash_id,
-                'csrf_token_check': self.token,
-                'mode': 'log',
-                'type': 'last',
-                'num': self.generate_current_timestamp(),
-            }
-
-            response = await loop.run_in_executor(None, partial(self.ses.post, 'https://c.kuku.lu/room.php', data=data))
-
-            return response.json()
+    async def create_room(self):
+        croom = await http_req.create_room()
+        print(croom)
+        self.rooms.append(Room(croom["hash"]))
+        return Room(croom["hash"])
--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import time
 bot = chatroom.WebSocket()
 
 @bot.command(name="test")
-async def test_command(*args):
-    print(await bot.send_room(f"あああ"))
+async def test_command(ctx: chatroom.Context, *args):
+    print(await ctx.room.send("こんばんは！"))
+
+@bot.command(name="create")
+async def create_command(ctx: chatroom.Context, *args):
+    room = await bot.create_room()
+    await ctx.room.send(room.url)
+    await bot.join_room(room)
 
 # イベント
 @bot.event
 async def on_chat(message: chatroom.Message):
     # メッセージ受信
     if message.content == "こんばんは":
-        await bot.send_room("こんばんは！")
+        await message.room.send("こんばんは！")
     await bot.process_command(message)
 
 # ログイン
-hash = bot.url_to_hash()
+hash = bot.url_to_hash("")
 
 bot.login(hash)
